a41a8c26f26412d8e9b931057e999e1f3d0ef117
nam666 <haquy490@gmail.com>
2023-04-09 17:38:42 +0700

    demo

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import App from './App.vue'
+import './index.css' // Import CSS của Tailwind
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+
+app.mount('#app')
+
+createApp(App).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography'),
+      require('@tailwindcss/aspect-ratio')
+    ],
+  }
+  
